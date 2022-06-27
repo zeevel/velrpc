c9393b42dd4f097c952a46a2d112228716925c82
zeevel <932690325@qq.com>
2022-06-27 15:05:55 +0800

    --story="header package"

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v0.0.4
+	github.com/stretchr/testify v1.7.5
 	google.golang.org/protobuf v1.28.0 // indirect
 )
--- header/pool.go
+++ header/pool.go
@@ -2,5 +2,16 @@ package header
 
 import "sync"
 
-var RequestPool sync.Pool
-var ResponsePool sync.Pool
+var (
+	RequestPool  sync.Pool
+	ResponsePool sync.Pool
+)
+
+func init() {
+	RequestPool = sync.Pool{New: func() interface{} {
+		return &RequestHeader{}
+	}}
+	ResponsePool = sync.Pool{New: func() interface{} {
+		return &ResponseHeader{}
+	}}
+}
