4146cb35b9abe31309c799f084187590fd6626a8
zeevel <932690325@qq.com>
2022-06-27 15:31:00 +0800

    --story="serializer and test.data package"

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/zeevel/velrpc
 go 1.15
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.2
 	github.com/golang/snappy v0.0.4
 	github.com/stretchr/testify v1.7.5
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.28.0
 )
---
+++ serializer/proto_serializer_test.go
@@ -0,0 +1,102 @@
+package serializer
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	pb "github.com/zeevel/velrpc/test.data/message"
+	"testing"
+)
+
+type test struct{}
+
+func TestProtoSerializer_Marshal(t *testing.T) {
+	type expect struct {
+		data []byte
+		err  error
+	}
+	cases := []struct {
+		name   string
+		arg    interface{}
+		expect expect
+	}{
+		{
+			name: "test-1",
+			arg:  &pb.ArithRequest{A: 1, B: 2},
+			expect: expect{
+				data: []byte{0x9, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0,
+					0x3f, 0x11, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x40},
+				err: nil,
+			},
+		},
+		{
+			name: "test-2",
+			arg:  test{},
+			expect: expect{
+				data: nil,
+				err:  errors.New("param does not implement proto.Message"),
+			},
+		},
+		{
+			name: "test-3",
+			arg:  nil,
+			expect: expect{
+				data: []byte{},
+				err:  nil,
+			},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			data, err := ProtoSerializer{}.Marshal(c.arg)
+			assert.Equal(t, c.expect.data, data)
+			assert.Equal(t, c.expect.err, err)
+		})
+	}
+}
+
+// TestProtoSerializer_Unmarshal .
+func TestProtoSerializer_Unmarshal(t *testing.T) {
+	type expect struct {
+		message interface{}
+		err     error
+	}
+	cases := []struct {
+		name    string
+		arg     []byte
+		message interface{}
+		expect  expect
+	}{
+		{
+			name: "test-1",
+			arg: []byte{0x9, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0xf0,
+				0x3f, 0x11, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x40},
+			message: &pb.ArithRequest{},
+			expect: expect{
+				message: &pb.ArithRequest{A: 1, B: 2},
+				err:     nil,
+			},
+		},
+		{
+			name:    "test-2",
+			arg:     nil,
+			message: nil,
+			expect: expect{
+				message: nil,
+				err:     nil,
+			},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			err := ProtoSerializer{}.Unmarshal(c.arg, c.message)
+			if err != nil {
+				assert.Equal(t, c.expect.message.(*pb.ArithRequest).A,
+					c.message.(*pb.ArithRequest).A)
+				assert.Equal(t, c.expect.message.(*pb.ArithRequest).B,
+					c.message.(*pb.ArithRequest).B)
+			}
+
+			assert.Equal(t, c.expect.err, err)
+		})
+	}
+}
